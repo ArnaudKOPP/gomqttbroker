cd538746ae06d80fd3fc3854b4854821e202d8b5
Arnaud KOPP <kopp.arnaud@gmail.com>
2019-08-13 22:35:19 +0200

    Add test client

--- main.go
+++ main.go
@@ -1,9 +1,3 @@
-/* Copyright (c) 2019, Arnaud KOPP
-
-Permission to use, copy, modify, and/or distribute this software for any
-purpose with or without fee is hereby granted, provided that the above
-copyright notice and this permission notice appear in all copies.
-*/
 package main
 
 import (
